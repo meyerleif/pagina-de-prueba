aa49b1b24d6c8cfb1d7c1aadc002dd538e071452
meyerleif <95261819+meyerleif@users.noreply.github.com>
2026-04-15 15:22:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function saludo() {
+    alert("Gracias por visitar mi página 👋");
+}
+
+function cambiarTexto() {
+    document.getElementById("texto").innerText = "¡Texto cambiado!";
+}
+function verProyecto() {
+    alert("Aquí puedes mostrar tu proyecto más adelante 🚀");
+}
